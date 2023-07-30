edb6806ec36e9f2a1498a08e5a16745c23405e3e
toofaniCoder <smartcoder500@gmail.com>
2023-07-30 17:28:28 +0530

    add server side pagiantion done

--- frontend/src/routes/users.jsx
+++ frontend/src/routes/users.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
   TableContainer,
   Table,
@@ -6,8 +7,17 @@ import {
   TableCell,
   TableBody,
   Paper,
+  Button,
+  Stack,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  TextField,
 } from '@mui/material';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useSearchParams } from 'react-router-dom';
 
 import {
   createColumnHelper,
@@ -15,6 +25,7 @@ import {
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table';
+import { useEffect, useState } from 'react';
 
 const columnHelper = createColumnHelper();
 
@@ -31,18 +42,66 @@ const columns = [
 ];
 
 const Users = () => {
-  const data = useLoaderData();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const users = useLoaderData();
   // log data: console.log('data from server:', data);
 
+  useEffect(() => {
+    console.log(pagination);
+    setSearchParams({
+      page: pagination.pageIndex + 1,
+      limit: pagination.pageSize,
+    });
+  }, [pagination]);
+
   const table = useReactTable({
-    data,
+    data: users.data,
     columns,
+    state: { pagination },
+    pageCount: users.totalCount / users.count,
+    onPaginationChange: setPagination,
+    manualPagination: true,
     getCoreRowModel: getCoreRowModel(),
   });
 
   return (
     <div>
       <TableContainer component={Paper}>
+        <Box component={Stack} direction={'row'} spacing={2} sx={{ p: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+              show items per page
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => {
+                table.setPageSize(Number(e.target.value));
+              }}
+              label="show items per page"
+            >
+              {[5, 10, 20, 25, 30, 40, 50].map((el, index) => (
+                <MenuItem key={index} value={el}>
+                  {el}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TextField
+            type="number"
+            value={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+            placeholder="go to page"
+          />
+        </Box>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -72,6 +131,26 @@ const Users = () => {
             ))}
           </TableBody>
         </Table>
+        <Stack sx={{ p: 2 }} direction={'row'} justifyContent={'space-between'}>
+          <Button
+            variant="contained"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            previous page
+          </Button>
+          <Typography>
+            Page {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            next page
+          </Button>
+        </Stack>
       </TableContainer>
     </div>
   );
