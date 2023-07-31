e0631da3e8229d8802258a54f4a4ba2df48046be
Subroto Biswas <37511552+toofaniCoder@users.noreply.github.com>
2023-07-31 18:10:41 +0530

    Revert "Server side pagination"

--- frontend/src/routes/users.jsx
+++ frontend/src/routes/users.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import {
   TableContainer,
   Table,
@@ -7,15 +6,8 @@ import {
   TableCell,
   TableBody,
   Paper,
-  Stack,
-  Button,
-  Typography,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
 } from '@mui/material';
-import { useLoaderData, useSearchParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 import {
   createColumnHelper,
@@ -39,56 +31,18 @@ const columns = [
 ];
 
 const Users = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [pagination, setPagination] = useState({
-    pageIndex: 0, // page index matlab = page number
-    pageSize: 5, // page size matlab = limit
-  });
-  const users = useLoaderData();
+  const data = useLoaderData();
   // log data: console.log('data from server:', data);
 
   const table = useReactTable({
-    data: users.data,
+    data,
     columns,
-    state: {
-      pagination,
-    },
-    pageCount: users.totalCount / users.count,
-    manualPagination: true,
-    onPaginationChange: setPagination,
     getCoreRowModel: getCoreRowModel(),
   });
 
-  useEffect(() => {
-    setSearchParams({
-      page: table.getState().pagination.pageIndex + 1,
-      limit: table.getState().pagination.pageSize,
-    });
-  }, [pagination]);
-  console.log(table.getState().pagination);
   return (
     <div>
       <TableContainer component={Paper}>
-        <Stack sx={{ p: 2 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">
-              show items per page
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={table.getState().pagination.pageSize}
-              label="show items per page"
-              onChange={(e) => table.setPageSize(Number(e.target.value))}
-            >
-              {[5, 10, 20, 40, 50, 100, 200, 500].map((el) => (
-                <MenuItem key={el} value={el}>
-                  {el}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Stack>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -118,28 +72,6 @@ const Users = () => {
             ))}
           </TableBody>
         </Table>
-        <Stack direction="row" justifyContent={'space-between'} sx={{ p: 3 }}>
-          <Button
-            disabled={!table.getCanPreviousPage()}
-            onClick={() => table.previousPage()}
-            color="primary"
-            variant="contained"
-          >
-            previous page
-          </Button>
-          <Typography>
-            Page {table.getState().pagination.pageIndex + 1} of{' '}
-            {table.getPageCount()}
-          </Typography>
-          <Button
-            disabled={!table.getCanNextPage()}
-            onClick={() => table.nextPage()}
-            color="primary"
-            variant="contained"
-          >
-            next page
-          </Button>
-        </Stack>
       </TableContainer>
     </div>
   );
